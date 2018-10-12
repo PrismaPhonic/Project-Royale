6b7b1b588c914cd4e6429519786017f03181083d
Peter Farr <Peter@PrismaPhonic.com>
2018-10-11 23:04:27 -0700

    removed commented out code - failed attempt to wait until dom loads to load youtube iframe from api function

--- static/script.js
+++ static/script.js
@@ -9,13 +9,6 @@ function toggleMuteIcon() {
   $('.fal').toggleClass('fa-volume-up fa-volume-slash')
 }
 
-// // YOUTUBE JAVASCRIPT CONTROL
-// window.player = undefined;
-
-// window.onYoutubeIframeAPIReady = function (callback) {
-//   callback();
-// };
-
 let player;
 
 function onYoutubeIframeAPIReady() {
@@ -41,29 +34,6 @@ function onYoutubeIframeAPIReady() {
 
 // JQUERY ON DOCUMENT LOAD
 $(document).ready(function () {
-
-  // //YOUTUBE CALLBACK ON DOM READY TO SPEED UP PAGE LOAD
-  // window.onYoutubeIframeAPIReady(function () {
-  //   window.player = new YT.Player('video-placeholder', {
-  //     width: '100%',
-  //     height: '100%',
-  //     videoId: 'iWxZiuTX_h4',
-  //     playerVars: {
-  //       color: 'white',
-  //       autoplay: 1,
-  //       controls: 0,
-  //     },
-  //     events: {
-  //       onReady: (event) => event.target.playVideo(),
-  //       onStateChange: function (e) {
-  //         if (e.data === YT.PlayerState.ENDED) {
-  //           window.player.playVideo();
-  //         }
-  //       },
-  //     }
-  //   });
-  // });
-
   // EVENT HANDLERS
   // NAVBAR LINK SCROLLING ANIMATION
   $('.navbar-nav').on('click', 'a', function (event) {
