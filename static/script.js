2f3c462c3c57df040c2454aaf2e5117f5b6f9565
Peter Farr <Peter@PrismaPhonic.com>
2018-10-11 20:41:15 -0700

    changed youtube api js to be local rather than external link

--- static/script.js
+++ static/script.js
@@ -1,40 +1,39 @@
-// UTILITY FUNCTIONS
-function scrollToSection(selector) {
-  $('html, body').animate({
-    scrollTop: $(selector).offset().top
-  }, 1500);
-};
-
-function toggleMuteIcon() {
-  $('.fal').toggleClass('fa-volume-up fa-volume-slash')
-}
-
-// YOUTUBE JAVASCRIPT CONTROL
-let player;
-
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('video-placeholder', {
-    width: '100%',
-    height: '100%',
-    videoId: 'iWxZiuTX_h4',
-    playerVars: {
-      color: 'white',
-      autoplay: 1,
-      controls: 0,
-    },
-    events: {
-      onReady: (event) => event.target.playVideo(),
-      onStateChange: function (e) {
-        if (e.data === YT.PlayerState.ENDED) {
-          player.playVideo();
-        }
-      },
-    }
-  });
-}
-
 // JQUERY ON DOCUMENT LOAD
 $(document).ready(function () {
+  // UTILITY FUNCTIONS
+  function scrollToSection(selector) {
+    $('html, body').animate({
+      scrollTop: $(selector).offset().top
+    }, 1500);
+  };
+
+  function toggleMuteIcon() {
+    $('.fal').toggleClass('fa-volume-up fa-volume-slash')
+  }
+
+  // YOUTUBE JAVASCRIPT CONTROL
+  let player;
+
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-placeholder', {
+      width: '100%',
+      height: '100%',
+      videoId: 'iWxZiuTX_h4',
+      playerVars: {
+        color: 'white',
+        autoplay: 1,
+        controls: 0,
+      },
+      events: {
+        onReady: (event) => event.target.playVideo(),
+        onStateChange: function (e) {
+          if (e.data === YT.PlayerState.ENDED) {
+            player.playVideo();
+          }
+        },
+      }
+    });
+  }
 
   // EVENT HANDLERS
   // NAVBAR LINK SCROLLING ANIMATION
