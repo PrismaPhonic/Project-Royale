174b5e751cef8996cd9cd8859a82fd32461fd579
Peter Farr <Peter@PrismaPhonic.com>
2018-10-11 22:39:06 -0700

    trying to fix youtube player - let's make sure it stil works not inside jquery dom ready

--- static/script.js
+++ static/script.js
@@ -9,37 +9,60 @@ function toggleMuteIcon() {
   $('.fal').toggleClass('fa-volume-up fa-volume-slash')
 }
 
-// YOUTUBE JAVASCRIPT CONTROL
-window.player = undefined;
+// // YOUTUBE JAVASCRIPT CONTROL
+// window.player = undefined;
 
-window.onYoutubeIframeAPIReady = function (callback) {
-  callback();
-};
+// window.onYoutubeIframeAPIReady = function (callback) {
+//   callback();
+// };
+
+let player;
+
+function onYoutubeIframeAPIReady() {
+  player = new YT.Player('video-placeholder', {
+    width: '100%',
+    height: '100%',
+    videoId: 'iWxZiuTX_h4',
+    playerVars: {
+      color: 'white',
+      autoplay: 1,
+      controls: 0,
+    },
+    events: {
+      onReady: (event) => event.target.playVideo(),
+      onStateChange: function (e) {
+        if (e.data === YT.PlayerState.ENDED) {
+          player.playVideo();
+        }
+      },
+    }
+  });
+});
 
 // JQUERY ON DOCUMENT LOAD
 $(document).ready(function () {
 
-  //YOUTUBE CALLBACK ON DOM READY TO SPEED UP PAGE LOAD
-  window.onYoutubeIframeAPIReady(function () {
-    window.player = new YT.Player('video-placeholder', {
-      width: '100%',
-      height: '100%',
-      videoId: 'iWxZiuTX_h4',
-      playerVars: {
-        color: 'white',
-        autoplay: 1,
-        controls: 0,
-      },
-      events: {
-        onReady: (event) => event.target.playVideo(),
-        onStateChange: function (e) {
-          if (e.data === YT.PlayerState.ENDED) {
-            window.player.playVideo();
-          }
-        },
-      }
-    });
-  });
+  // //YOUTUBE CALLBACK ON DOM READY TO SPEED UP PAGE LOAD
+  // window.onYoutubeIframeAPIReady(function () {
+  //   window.player = new YT.Player('video-placeholder', {
+  //     width: '100%',
+  //     height: '100%',
+  //     videoId: 'iWxZiuTX_h4',
+  //     playerVars: {
+  //       color: 'white',
+  //       autoplay: 1,
+  //       controls: 0,
+  //     },
+  //     events: {
+  //       onReady: (event) => event.target.playVideo(),
+  //       onStateChange: function (e) {
+  //         if (e.data === YT.PlayerState.ENDED) {
+  //           window.player.playVideo();
+  //         }
+  //       },
+  //     }
+  //   });
+  // });
 
   // EVENT HANDLERS
   // NAVBAR LINK SCROLLING ANIMATION
